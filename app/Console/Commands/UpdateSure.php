08513e819de83c4ff227ee21d3ddcc46e2686dd6
lawalthb <lawalthb@gmail.com>
2025-04-22 20:01:21 +0100

    new commad added

---
+++ app/Console/Commands/UpdateSure.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Setting;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class UpdateSure extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'pusher:sure';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Update Pusher settings to debby details';
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+
+
+    // Get values for the new set from settings
+    $newAppId = Setting::get("pusher_app_id_5");
+    $newKey = Setting::get("pusher_key_5");
+    $newSecret = Setting::get("pusher_secret_5");
+    $newEmail = Setting::get("pusher_email_5");
+
+    // Update active credentials
+    Setting::updateOrCreate(['key' => 'pusher_app_id'], ['value' => $newAppId]);
+    Setting::updateOrCreate(['key' => 'pusher_key'], ['value' => $newKey]);
+    Setting::updateOrCreate(['key' => 'pusher_secret'], ['value' => $newSecret]);
+    Setting::updateOrCreate(['key' => 'pusher_email'], ['value' => $newEmail]);
+
+    // Update the active set indicator
+    Setting::updateOrCreate(['key' => 'active_pusher_set'], ['value' => 5]);
+
+    $this->info("Switched to Pusher credentials set sure");
+  }
+}
